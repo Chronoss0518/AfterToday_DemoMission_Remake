9046fba4b542f5380dcb8dcba20be3b574d2fae3
Chronoss <sinyusuke5000@gmail.com>
2023-04-22 16:00:25 +0900

    CameraをComponentへ変更

---
+++ Src/BaseMecha/FunctionComponent/CameraComponent.cpp
@@ -0,0 +1,10 @@
+
+#include"../../BaseIncluder.h"
+
+#include"../../AllStruct.h"
+
+#include"../../Attack/Attack.h"
+#include"../MechaPartsObject.h"
+
+#include"CameraComponent.h"
+#include"../BaseMecha.h"
---
+++ Src/BaseMecha/FunctionComponent/CameraComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class CameraObject {
+
+public:
+
+	inline void SetCameraPositionObject(ChPtr::Shared<MechaPartsObject>& _cameraPos)
+	{
+		cameraPos = _cameraPos;
+	}
+
+private:
+
+	ChPtr::Shared<MechaPartsObject>cameraPos = nullptr;
+
+	bool 
+
+};
