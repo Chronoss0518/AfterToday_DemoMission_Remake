929c375eb00a294f89f8ec7bf4917f45bc6a8538
Chronoss <sinyusuke5000@gmail.com>
2023-07-09 17:25:08 +0900

    ※コミット漏れ:カメラの方向を変更する機能の修正

--- Src/BaseMecha/FunctionComponent/MoveComponent.cpp
+++ Src/BaseMecha/FunctionComponent/MoveComponent.cpp
@@ -9,8 +9,8 @@ void MoveComponentBase::Update()
 {
 	ChLMat tmp;
 
+	//tmp.SetRotationXAxis(ChMath::ToRadian(GetRotation().x));
 	tmp.SetRotationYAxis(ChMath::ToRadian(GetRotation().y));
-	tmp.SetRotationXAxis(ChMath::ToRadian(GetRotation().x));
 
 	MoveUpdate(movePow, InputName::Front, InputName::FrontBoost, InputName::FrontAvo, ChVec3(0.0f, 0.0f, 1.0f), tmp);
 	MoveUpdate(movePow, InputName::Back, InputName::BackBoost, InputName::BackAvo, ChVec3(0.0f, 0.0f, -1.0f), tmp);
@@ -18,48 +18,17 @@ void MoveComponentBase::Update()
 	MoveUpdate(movePow, InputName::Right, InputName::RightBoost, InputName::RightAvo, ChVec3(1.0f, 0.0f, 0.0f), tmp);
 	MoveUpdate(jumpPow, InputName::Jump, InputName::UpBoost, InputName::UpAvo, ChVec3(0.0f, 1.0f, 0.0f), tmp);
 
-	RotateUpdate(rotatePow, InputName::UpRotation, ChVec3(1.0f, 0.0f, 0.0f));
-	RotateUpdate(rotatePow, InputName::DownRotation, ChVec3(-1.0f, 0.0f, 0.0f));
-	RotateUpdate(rotatePow, InputName::RightRotation, ChVec3(0.0f, -1.0f, 0.0f));
-	RotateUpdate(rotatePow, InputName::LeftRotation, ChVec3(0.0f, 1.0f, 0.0f));
-
 	CamVerticalRotateUpdate(InputName::CameraUpRotation, cameraRotatePow);
 	CamVerticalRotateUpdate(InputName::CameraDownRotation, -cameraRotatePow);
 
 	CamHorizontalRotateUpdate(InputName::CameraRightRotation, cameraRotatePow);
 	CamHorizontalRotateUpdate(InputName::CameraLeftRotation, -cameraRotatePow);
-}
 
-void MoveComponentBase::SetUsingCameraRotateAxisFlg(bool _flg, MoveComponentBase::UsingCameraRotateAxisType _type)
-{
-	usingCameraRotateAxisFlg.SetBitFlg(ChStd::EnumCast(_type), _flg);
-}
+	RotateUpdate(rotatePow, InputName::UpRotation, ChVec3(1.0f, 0.0f, 0.0f));
+	RotateUpdate(rotatePow, InputName::DownRotation, ChVec3(-1.0f, 0.0f, 0.0f));
+	RotateUpdate(rotatePow, InputName::RightRotation, ChVec3(0.0f, -1.0f, 0.0f));
+	RotateUpdate(rotatePow, InputName::LeftRotation, ChVec3(0.0f, 1.0f, 0.0f));
 
-void MoveComponentBase::CamVerticalRotateUpdate(InputName _input, const float _camRot)
-{
-	if (!IsPushFlg(_input))return;
-	
-	if (!usingCameraRotateAxisFlg.GetBitFlg(ChStd::EnumCast(UsingCameraRotateAxisType::Vertical)))return;
-
-	AddViewRotateVertical(_camRot / PhysicsMachine::GetFPS());
-}
-
-void MoveComponentBase::CamHorizontalRotateUpdate(InputName _input, const float _camRot)
-{
-	if (!IsPushFlg(_input))return;
-
-	if (!usingCameraRotateAxisFlg.GetBitFlg(ChStd::EnumCast(UsingCameraRotateAxisType::Horizontal)))return;
-
-	AddViewRotateHorizontal(_camRot / PhysicsMachine::GetFPS());
-}
-
-
-void BaseMechaMoveComponent::Init()
-{
-	FunctionComponent::Init();
-
-	SetUsingCameraRotateAxisFlg(false, UsingCameraRotateAxisType::Horizontal);
-	SetUsingCameraRotateAxisFlg(true, UsingCameraRotateAxisType::Vertical);
 }
 
 void BaseMechaMoveComponent::MoveUpdate(float _pow, InputName _input, InputName _boost, InputName _avoid, const ChVec3& _direction, const ChLMat& _nowTargetPoster)
@@ -83,5 +52,25 @@ void BaseMechaMoveComponent::RotateUpdate(float _pow, InputName _input, const Ch
 
 	if (!IsPushFlg(_input))return;
 
-	AddRotateVector(_direction * _pow);
+	auto vector = _direction;
+
+	vector *= _pow;
+
+	AddRotateVector(vector);
+}
+
+void BaseMechaMoveComponent::CamVerticalRotateUpdate(InputName _input, const float _camRot)
+{
+	if (!IsPushFlg(_input))return;
+
+	AddViewRotateVertical(_camRot / PhysicsMachine::GetFPS());
+}
+
+void BaseMechaMoveComponent::CamHorizontalRotateUpdate(InputName _input, const float _camRot)
+{
+	if (!IsPushFlg(_input))return;
+
+	if (_input == InputName::CameraRightRotation)SetPushFlg(InputName::RightRotation);
+
+	if (_input == InputName::CameraLeftRotation)SetPushFlg(InputName::LeftRotation);
 }
