c92124e309d736f3d8dcc6c85de8ab750400da20
Chronoss <sinyusuke5000@gmail.com>
2023-08-03 20:02:48 +0900

    PhysicsMachineを別の場所で一括で操作できるようにするため削除

--- Src/Physics/Physics_Gravity.cpp
+++
@@ -1,9 +0,0 @@
-#include"../BaseIncluder.h"
-#include"../AllStruct.h"
-
-#include"Physics_Gravity.h"
-
-void Physics_Gravity::AddMove()
-{
-	AddMovePowerVector(ChVec3(0.0f, -(GetGravityAcceleration() * GetMass()), 0.0f));
-}
