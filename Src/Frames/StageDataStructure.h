b84ecacbd27e77cc7f0446595b43e1219812a472
Chronoss0518 <sinyusuke5000@gmail.com>
2023-12-23 18:35:25 +0900

    ResultDataStructureにコメントを追加

--- Src/Frames/StageDataStructure.h
+++ Src/Frames/StageDataStructure.h
@@ -2,22 +2,29 @@
 
 struct ResultStructure :public ChCpp::SendDataClass
 {
-
+	//¬Œ÷•ñV//
 	unsigned long successFee;
+	//’Ç‰Á•ñV//
 	unsigned long additionalCompensation;
+	//“Á•ÊŒ¸‹‹//
 	unsigned long specialPayReduction;
+	//C‘U”ï//
 	unsigned long repairCosts;
+	//’e–ò”ï//
 	unsigned long ammunitionExpense;
+	//‹Ÿ‹‹”ï//
 	unsigned long energyCosts;
 
+	//’Ç‰Á•ñVà–¾//
 	std::string additionalCompensationExplanation;
+	//“Á•ÊŒ¸‹‹à–¾//
 	std::string specialPayReductionExplanation;
-
 };
 
 struct StageDataStructure : public ChCpp::SendDataClass
 {
 	ChPtr::Shared<ResultStructure> resultData = ChPtr::Make_S<ResultStructure>();
 
-	std::string stageName = "";
+	//StageScriptPath//
+	std::string stageScriptPath = "";
 };
