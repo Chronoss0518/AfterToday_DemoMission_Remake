9a7d2ebfec656f763f11ac7bc95fb4f28858b27e
Chronoss0518 <sinyusuke5000@gmail.com>
2024-01-07 16:49:59 +0900

    StageDataにあったStageDataStructureのSave・Load機能をStageDataStructureに移植したため削除

--- Src/StageSelectFrame/StageData/StageData.cpp
+++ Src/StageSelectFrame/StageData/StageData.cpp
@@ -4,84 +4,14 @@
 
 void StageData::LoadData(const std::string& _stageFilePath)
 {
-	std::string text = "";
+	stageDatas->Load(_stageFilePath);
 
-	{
-		ChCpp::CharFile file;
-		file.FileOpen(_stageFilePath);
-		text = file.FileReadText();
-		file.FileClose();
-	}
+	if (!stageDatas->stageImagePath.empty())
+		stageImage.CreateTexture(stageDatas->stageImagePath);
 
-	auto jsonObject = ChPtr::SharedSafeCast<ChCpp::JsonObject>(ChCpp::JsonBaseType::GetParameter(text));
-	
-	if (jsonObject == nullptr)return;
-
-	auto stagePathJson = jsonObject->GetJsonString("StagePath");
-	if (stagePathJson != nullptr)stageDatas->stageName = stagePathJson->GetString();
-
-	auto stageNameJson = jsonObject->GetJsonString("StageName");
-	if (stageNameJson != nullptr)stageName = ChStr::UTF8ToWString(stageNameJson->GetString());
-
-	auto stageImageJson = jsonObject->GetJsonString("StageImage");
-	if (stageImageJson != nullptr)
-	{
-		stageImage.path = stagePathJson->GetString();
-		stageImage.image.CreateTexture(stageImage.path);
-	}
-
-	auto targetNameJson = jsonObject->GetJsonString("TargetName");
-	if (targetNameJson != nullptr)targetName = ChStr::UTF8ToWString(targetNameJson->GetString());
-
-	auto targetImageList = jsonObject->GetJsonArray("TargetImags");
-	if (targetImageList != nullptr)
-	{
-		for (unsigned long i = 0; i < targetImageList->GetCount(); i++)
-		{
-			auto targetImageJson = targetImageList->GetJsonString(i);
-			if (targetImageJson == nullptr)continue;
-			targetImages[i] = ChPtr::Make_S<UseImageData>();
-			targetImages[i]->path = targetImageJson->GetString();
-			targetImages[i]->image.CreateTexture(targetImages[i]->path);
-		}
-	}
 }
 
 void StageData::SaveData(const std::string& _stageFilePath)
 {
-	auto res = ChPtr::Make_S<ChCpp::JsonObject>();
-
-	res->SetObject("StageScriptPath", ChCpp::JsonString::CreateObject(stageDatas->stageScriptPath));
-	res->SetObject("SuccessFee", ChCpp::JsonNumber::CreateObject(stageDatas->resultData->successFee));
-	res->SetObject("AdditionalCompensation", ChCpp::JsonNumber::CreateObject(stageDatas->resultData->additionalCompensation));
-	res->SetObject("AdditionalCompensationExplanation", ChCpp::JsonString::CreateObject(stageDatas->resultData->additionalCompensationExplanation));
-	res->SetObject("SpecialPayReduction", ChCpp::JsonNumber::CreateObject(stageDatas->resultData->specialPayReduction));
-	res->SetObject("SpecialPayReductionExplanation", ChCpp::JsonString::CreateObject(stageDatas->resultData->specialPayReductionExplanation));
-
-	res->SetObject("StageName", ChCpp::JsonString::CreateObject(ChStr::UTF8ToString(stageName)));
-
-	res->SetObject("StageImage", ChCpp::JsonString::CreateObject(stageImage.path));
-
-	auto targetImageList = ChPtr::Make_S<ChCpp::JsonArray>();
-
-	for (unsigned char i = 0; i < 10; i++)
-	{
-		if (targetImages[i] == nullptr)continue;
-
-		targetImageList->AddObject(ChCpp::JsonString::CreateObject(targetImages[i]->path));
-
-	}
-
-	res->SetObject("TargetImages", targetImageList);
-
-	res->SetObject("TargetName", ChCpp::JsonString::CreateObject(ChStr::UTF8ToString(targetName)));
-
-
-	{
-		ChCpp::CharFile file;
-		file.FileOpen(_stageFilePath);
-		auto result = file.FileWriteText(res->GetRawData());
-		file.FileClose();
-	}
-
+	stageDatas->Save(_stageFilePath);
 }
--- Src/StageSelectFrame/StageData/StageData.h
+++ Src/StageSelectFrame/StageData/StageData.h
@@ -4,12 +4,6 @@
 
 class StageData
 {
-	struct UseImageData
-	{
-		ChD3D11::Texture11 image;
-		std::string path = "";
-	};
-
 public:
 
 	void LoadData(const std::string& _stageFilePath);
@@ -22,16 +16,9 @@ public:
 
 	ChPtr::Shared<StageDataStructure>stageDatas = ChPtr::Make_S<StageDataStructure>();
 
-	std::wstring stageName = L"";
 	ChPtr::Shared<ChD3D11::Texture11>stageNameText = nullptr;
 
-	UseImageData stageImage;
-
-	std::wstring targetName = L""; 
-	ChPtr::Shared<ChD3D11::Texture11>targetNameText = nullptr;
-
-	ChPtr::Shared<UseImageData> targetImages[10];
-
+	ChD3D11::Texture11 stageImage;
 	
 };
 
