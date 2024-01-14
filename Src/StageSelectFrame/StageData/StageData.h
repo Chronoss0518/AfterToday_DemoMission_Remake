bd94442a410e7e066ac5b4635d257360cf67d5c8
Chronoss0518 <sinyusuke5000@gmail.com>
2024-01-14 19:26:46 +0900

    StageData側で保持する画像をSharedPtrから実変数へ変更

--- Src/StageSelectFrame/StageData/StageData.h
+++ Src/StageSelectFrame/StageData/StageData.h
@@ -16,9 +16,9 @@ public:
 
 	ChPtr::Shared<StageDataStructure>stageDatas = ChPtr::Make_S<StageDataStructure>();
 
-	ChPtr::Shared<ChD3D11::RenderTarget11>selectPanel = nullptr;
+	ChD3D11::Texture11 selectPanel;
 
-	ChPtr::Shared<ChD3D11::Texture11>description = nullptr;
+	ChD3D11::Texture11 description;
 
 	ChD3D11::Texture11 stageImage;
 	
