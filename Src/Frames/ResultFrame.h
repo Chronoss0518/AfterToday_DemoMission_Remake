079bd865bab92d15927874d3101a91a947dd373d
Chronoss0518 <sinyusuke5000@gmail.com>
2024-01-23 21:08:54 +0900

    Result画面の配置を修正

--- Src/Frames/ResultFrame.h
+++ Src/Frames/ResultFrame.h
@@ -1,5 +1,7 @@
 #pragma once
 
+struct ResultStructure;
+
 class ResultFrame : public ChCpp::BaseFrame
 {
 public:
@@ -12,41 +14,110 @@ public:
 
 private:
 
-	struct DrawBlock
+	static constexpr unsigned long EXPLANATION_TYPE_COUNT = 2;
+	enum class ExplanationType
 	{
-
-		ChD3D11::Texture11 block;
-		ChD3D11::Sprite11 drawPosition;
+		AdditionalCompensation,
+		SpecialPayReduction
 	};
 
-	struct DrawValueBlock : public DrawBlock
+	static constexpr unsigned long BLOCK_TYPE_COUNT = 7;
+	enum class BlockType
 	{
-
-		ChPtr::Shared<ChD3D11::Texture11> value = nullptr;
+		SuccessFee,
+		AdditionalCompensation,
+		SpecialPayReduction,
+		RepairCosts,
+		AmmunitionExpense,
+		EnergyCosts,
+		FinalIncome,
 	};
+
+
+	static constexpr unsigned long SELECT_BUTTON_TYPE_COUNT = 2;
+	enum class ActionType
+	{
+		LeftSelect,
+		RightSelect,
+		Decision
+	};
+
+private:
+
+	struct DrawValueBlock
+	{
+		ImageSprite back;
+		ImageSprite value;
+	};
+
+	struct DrawExplanationImage
+	{
+		ChD3D11::Texture11 backGround;
+		ChD3D11::Texture11 text;
+	};
+
+	struct ButtonImage : public ImageSprite
+	{
+		ChD3D11::Texture11 select;
+	};
+
+private:
 
 	void UpdateFunction();
 
 	void DrawFunction();
 
-	ChPtr::Shared<ChD3D11::Texture11> CreateTextImageFromPayment(
-		unsigned long _value,
+	void UpdateMouse();
+
+	void UpdateKeyboard();
+
+	void UpdateController();
+
+	void CreateBlockData(ID3D11Device* _device, const ResultStructure& _result);
+
+	void CreateExplanationData(ID3D11Device* _device, const ResultStructure& _result);
+
+	void CreateTextImageFromBlock(
+		ID3D11Device* _device,
+		const std::wstring& _value,
+		ChD3D11::Texture11& _outImage,
 		ChD3D::DirectFontBase& _drawer,
 		ChD3D::WICBitmapObject& _bitmap, 
 		ChD3D::BrushObject& _brush,
 		ChD3D::TextFormatObject& _format);
 
-	DrawBlock resultTitle;
+	void CreateTextImageFromExplanation(
+		ID3D11Device* _device,
+		const std::wstring& _value,
+		ChD3D11::Texture11& _outImage,
+		ChD3D::DirectFontBase& _drawer,
+		ChD3D::WICBitmapObject& _bitmap,
+		ChD3D::BrushObject& _brush,
+		ChD3D::TextFormatObject& _format);
 
-	DrawValueBlock successFee;
-	DrawValueBlock additionalCompensation;
-	DrawValueBlock specialPayReduction;
-	DrawValueBlock repairCosts;
-	DrawValueBlock ammunitionExpense;
-	DrawValueBlock energyCosts;
-	DrawValueBlock finalIncome;
+private:
 
-	DrawValueBlock additionalCompensationExplanation;
-	DrawValueBlock specialPayReductionExplanation;
+	std::vector<ActionType> inputDataList;
 
+	ChPtr::Shared<ResultStructure> resultData = nullptr;
+	long finalIncome = 0;
+
+	ChD3D::XInputController controller;
+	ChCpp::BitBool conntrollerPushKey;
+	bool initFlg = false;
+
+	ChD3D11::Shader::BaseDrawSprite11 spriteShader;
+
+	ImageSprite resultTitle;
+
+	DrawValueBlock block[BLOCK_TYPE_COUNT];
+
+	int exprlationType = 0;
+	ChD3D11::Sprite11 explanationBackGroundSprite;
+	ChD3D11::Sprite11 explanationTextSprite;
+	DrawExplanationImage explanation[EXPLANATION_TYPE_COUNT];
+
+	int buttonSelectType = ChStd::EnumCast(ActionType::Decision);
+	ButtonImage button[SELECT_BUTTON_TYPE_COUNT];
+	ChWin::MouseController* mouse = &ChWin::Mouse();
 };
