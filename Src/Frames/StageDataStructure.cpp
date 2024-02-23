66887bee10dacc1f8baeb85cb18740430b20fa3b
Chronoss0518 <sinyusuke5000@gmail.com>
2024-02-23 17:43:41 +0900

    不要な機能を削除

--- Src/Frames/StageDataStructure.cpp
+++ Src/Frames/StageDataStructure.cpp
@@ -45,36 +45,9 @@ void StageDataStructure::Load(const std::string& _filePath)
 
 	auto stageScriptPathJson = jsonObject->GetJsonString(J_STAGE_SCRIPT_PATH);
 	if (stageScriptPathJson != nullptr)stageScriptPath = stageScriptPathJson->GetString();
-	std::string test = "";
 
 	auto stageStrategyOverviewJson = jsonObject->GetJsonString(J_STAGE_STRATEGY_OVERVIEW);
-	if (stageStrategyOverviewJson != nullptr)test = stageStrategyOverviewJson->GetString();
-
-	std::string u8crlf = ChStr::UTF8ToString(L"\n");
-	
-	bool crlfFlg = true;
-
-	for (unsigned long i = 0; i < test.size();i++)
-	{
-		crlfFlg = true;
-		
-		for (unsigned long j = 0; j < u8crlf.size(); j++)
-		{
-			if (test[i] == u8crlf[j])continue;
-			crlfFlg = false;
-			break;
-		}
-		if (crlfFlg)
-		{
-			stageStrategyOverview += "\n";
-
-			i += u8crlf.length();
-
-			continue;
-		}
-
-		stageStrategyOverview += test[i];
-	}
+	if (stageStrategyOverviewJson != nullptr)stageStrategyOverview = stageStrategyOverviewJson->GetString();
 
 	auto missionTimeSeccondJson = jsonObject->GetJsonNumber(J_STAGE_MISSION_TIME);
 	if (missionTimeSeccondJson != nullptr)missionTimeSeccond = *missionTimeSeccondJson;
