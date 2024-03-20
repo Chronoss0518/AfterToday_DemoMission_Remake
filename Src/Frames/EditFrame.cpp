df6835dbacc00b8bc356942685fce38467d1dda2
Chronoss0518 <sinyusuke5000@gmail.com>
2024-03-20 15:00:30 +0900

    不要となった部分の削除

--- Src/Frames/EditFrame.cpp
+++ Src/Frames/EditFrame.cpp
@@ -12,82 +12,20 @@
 #define EDIT_TEXTURE_DIRECTORY(current_path) TEXTURE_DIRECTORY("Edit/") current_path
 #define EDIT_MESH_DIRECTORY(current_path) MESH_DIRECTORY("Edit/") current_path
 
-
-
-constexpr char const* initTitle = "non title";
-
-
 void EditFrame::Init(ChPtr::Shared<ChCpp::SendDataClass> _sendData)
 {
 	ChD3D11::Shader11().SetBackColor(ChVec4::FromColor(0.0f, 0.0f, 0.0f, 1.0f));
 
+	controller.Init();
+
+	MenuBase::InitMenu(&controller);
+
 	auto&& device = ChD3D11::D3D11Device();
-	drawTexter.Init(ChD3D11::D3D11API().GetSurface());
-
-	spriteShader.Init(device);
-	partsTitlePanel.CreateTexture(EDIT_TEXTURE_DIRECTORY("PanelList.png"), device);
-
-	addMecha.CreateTexture("", device);
-	
-	panelBackGround.CreateTexture(EDIT_TEXTURE_DIRECTORY("PanelList.png"), device);
-
-	auto&& leftPos = ChVec2(30, 30);
-	auto&& rightPos = ChVec2(970, 30);
-	auto&& size = ChVec2(280, 660);
-
-	auto&& leftPanelRect = RectToGameWindow(ChVec4::FromRect(leftPos.x, leftPos.y, leftPos.x + size.x, leftPos.y + size.y));
-	auto&& rightPanelRect = RectToGameWindow(ChVec4::FromRect(rightPos.x, rightPos.y, rightPos.x + size.x, rightPos.y + size.y));
-
-	leftPanel = ChPtr::Make_S<PanelList>();
-	leftPanel->Init(ChD3D11::D3D11Device(), drawTexter);
-	leftPanel->SetPanelListBackGround(&panelBackGround, leftPanelRect);
-
-	rightPanel = ChPtr::Make_S<PanelList>();
-	rightPanel->Init(ChD3D11::D3D11Device(), drawTexter);
-	rightPanel->SetPanelListBackGround(&panelBackGround, rightPanelRect);
-
-	panelSprite.Init();
-	panelSprite.SetInitPosition();
 }
 
 void EditFrame::Release()
 {
 
-}
-
-void EditFrame::UpdateFunction()
-{
-	auto&& windows = ChSystem::SysManager().GetSystem<ChSystem::Windows>();
-
-	if (windows->IsPushKey(VK_ESCAPE))
-	{
-		PostQuitMessage(0);
-		return;
-	}
-
-
-
-
-	SetInitFlg(true);
-}
-
-void EditFrame::DrawFunction()
-{
-	ChD3D11::Shader11().DrawStart();
-
-	spriteShader.DrawStart(ChD3D11::D3D11DC());
-
-	//drawTexter.DrawStart();
-
-	leftPanel->Draw(spriteShader, drawTexter);
-
-	rightPanel->Draw(spriteShader, drawTexter);
-
-	//drawTexter.DrawEnd();
-
-	spriteShader.DrawEnd();
-
-	ChD3D11::Shader11().DrawEnd();
 }
 
 void EditFrame::Update()
@@ -97,56 +35,21 @@ void EditFrame::Update()
 	DrawFunction();
 }
 
-void EditFrame::CreateNewFile()
-{
-	editMecha = CreateMecha();
-	
-	fileName = initTitle;
-
-}
-
-void EditFrame::CreateLoadFileList()
-{
-
-
-}
-
-void EditFrame::LoadFile(const std::string& _fileName)
-{
-	editMecha = CreateMecha();
-
-	editMecha->Load(ChD3D11::D3D11Device(), _fileName);
-
-	fileName = _fileName;
-}
-
-void EditFrame::SaveFile()
-{
-	if (editMecha == nullptr)return;
-
-	editMecha->Save(fileName);
-}
-
-void EditFrame::AddPartsObject(MechaParts::PartsPosNames _name, ChPtr::Shared<MechaPartsObject> _createParts)
+void EditFrame::UpdateAction(ActionType _type)
 {
 
 }
 
-void EditFrame::ChangePartsObejct(MechaParts::PartsPosNames _name, ChPtr::Shared<MechaPartsObject> _createParts)
+void EditFrame::UpdateMouse()
 {
 
 }
 
-void EditFrame::RemovePartsObejct(MechaParts::PartsPosNames _name, ChPtr::Shared<MechaPartsObject> _createParts)
+void EditFrame::DrawFunction()
 {
+	ChD3D11::Shader11().DrawStart();
 
-}
 
-ChPtr::Shared<BaseMecha> EditFrame::CreateMecha()
-{
-	auto&& mecha = ChPtr::Make_S<BaseMecha>();
 
-	mecha->Create(ChVec2(GAME_WINDOW_WIDTH, GAME_WINDOW_HEIGHT), meshDrawer, nullptr);
-
-	return mecha;
+	ChD3D11::Shader11().DrawEnd();
 }
--- Src/Frames/EditFrame.h
+++ Src/Frames/EditFrame.h
@@ -3,20 +3,13 @@
 class MechaParts;
 class MechaPartsObject;
 
-#include"../BaseMecha/MechaPartsObject.h"
 
-class PanelList;
 
-class EditFrame :public ChCpp::BaseFrame,public ChCp::Initializer
+#include"MenuBase.h"
+
+
+class EditFrame :public ChCpp::BaseFrame, public MenuBase
 {
-private:
-
-	struct MouseData
-	{
-		bool flg;
-		ChVec2 position;
-	};
-
 public:
 
 	void Init(ChPtr::Shared<ChCpp::SendDataClass> _sendData)override;
@@ -25,62 +18,16 @@ public:
 
 	void Update()override;
 
-public:
-
-	inline void SetFileName(const std::string& _fileName)
-	{
-		fileName = _fileName;
-	}
-
-public:
-
-	void AddPartsObject(MechaParts::PartsPosNames _name, ChPtr::Shared<MechaPartsObject> _createParts);
-
-	void ChangePartsObejct(MechaParts::PartsPosNames _name, ChPtr::Shared<MechaPartsObject> _createParts);
-
-	void RemovePartsObejct(MechaParts::PartsPosNames _name, ChPtr::Shared<MechaPartsObject> _createParts);
-
-
-public:
-
-	void CreateNewFile();
-
-	void CreateLoadFileList();
-
-	void LoadFile(const std::string& _fileName);
-
-	void SaveFile();
-
 private:
 
 	void DrawFunction();
 
-	void UpdateFunction();
+	void UpdateAction(ActionType _type)override;
 
-	ChD3D11::Texture11 loadTitlePanel, partsTitlePanel;
-	ChD3D11::Texture11 partsTitlePanelList[ChStd::EnumCast(MechaParts::PartsPosNames::None)];
+	void UpdateMouse()override;
 
-	ChD3D11::Texture11 *addButtonTexture = nullptr;
+private:
 
-	ChD3D11::Texture11 addMecha, addParts[ChStd::EnumCast(MechaParts::PartsPosNames::None)];
-	
-	ChD3D11::Sprite11 panelSprite;
-	ChVec2 panelSize = ChVec2();
-	float padding = 0.0f;
+	ChD3D::XInputController controller;
 
-	ChPtr::Shared<PanelList>leftPanel = nullptr, rightPanel = nullptr;
-	ChD3D11::Texture11 panelBackGround;
-	
-	ChPtr::Shared<BaseMecha> CreateMecha();
-
-	ChD3D11::Shader::BaseDrawSprite11 spriteShader;
-
-	ChPtr::Shared<BaseMecha>editMecha = nullptr;
-	ChD3D11::Shader::BaseDrawMesh11 meshDrawer;
-	ChD3D::DirectFontFromDXGISurface drawTexter;
-
-	std::string fileName = "";
-	std::vector<std::filesystem::path> fileList;
-
-	MouseData downData, upData;
 };
