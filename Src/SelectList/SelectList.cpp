506424bd3b2878530af5eae39902b6841e0eb09e
Chronoss0518 <sinyusuke5000@gmail.com>
2024-03-23 15:15:12 +0900

    選択するためのリストパネルを管理するクラスを構築

--- Src/SelectList/SelectList.cpp
+++ Src/SelectList/SelectList.cpp
@@ -3,12 +3,187 @@
 #include"SelectList.h"
 
 
-bool SelectListBase::Update(ChWin::MouseController& _controller)
+void SelectListBase::SetStartPosition(float _x, float _y)
 {
+	SetStartPosition(ChVec2(_x, _y));
+}
+
+void SelectListBase::SetStartPosition(const ChVec2& _position)
+{
+	startPosition = _position;
+}
+
+void SelectListBase::SetPanelSize(float _x, float _y)
+{
+	SetPanelSize(ChVec2(_x, _y));
+}
+
+void SelectListBase::SetPanelSize(const ChVec2& _size)
+{
+	basePanelSize = _size;
+}
+
+void SelectListBase::SetAlighSize(float _x, float _y)
+{
+	SetAlighSize(ChVec2(_x, _y));
+}
+
+void SelectListBase::SetAlighSize(const ChVec2& _size)
+{
+	alighSize = _size;
+}
+
+bool SelectListBase::UpdateMouse()
+{
+	ChVec4 rect = GetStartRect();
+
+	for (unsigned long i = drawPos; i < drawCount + drawPos; i++)
+	{
+		if (IsMoucePosOnRect(rect))
+		{
+			nowSelectPanel = i % itemList.size();
+			return true;
+		}
+
+		rect = GetNextRect(rect);
+	}
+
 	return false;
+}
+
+void SelectListBase::UpdateAction(MenuBase::ActionType _type)
+{
+	UpdateActionVertical(_type);
+
+	UpdateActionHorizontal(_type);
+
+	return;
+}
+
+void SelectListBase::UpdateActionVertical(MenuBase::ActionType _type)
+{
+	if (moveDirection != MoveDiraction::Vertical)return;
+
+	if (_type == MenuBase::ActionType::Up)
+	{
+		MoveSub();
+	}
+
+	if (_type == MenuBase::ActionType::Down)
+	{
+		MoveAdd();
+	}
+}
+
+void SelectListBase::UpdateActionHorizontal(MenuBase::ActionType _type)
+{
+	if (moveDirection != MoveDiraction::Horizontal)return;
+
+	if (_type == MenuBase::ActionType::Right)
+	{
+		MoveAdd();
+	}
+
+	if (_type == MenuBase::ActionType::Left)
+	{
+		MoveSub();
+	}
 }
 
 void SelectListBase::Draw(ChD3D11::Shader::BaseDrawSprite11& _drawer)
 {
+	ChVec4 rect = GetStartRect();
 
+	for (unsigned long i = drawPos;i<drawCount + drawPos;i++)
+	{
+		DrawPanel(_drawer, rect, itemList[i % itemList.size()],nowSelectPanel == (i % itemList.size()));
+
+		rect = GetNextRect(rect);
+	}
+}
+
+void SelectListBase::AddItem(ChPtr::Shared<SelectListItemBase> _item)
+{
+	if (_item == nullptr)return;
+	itemList.push_back(_item);
+}
+
+void SelectListBase::RemoveItem(ChPtr::Shared<SelectListItemBase> _item)
+{
+	if (_item == nullptr)return;
+
+	auto&& it = std::find(itemList.begin(), itemList.end(), _item);
+	if (it == itemList.end())return;
+
+	itemList.erase(it);
+}
+
+void SelectListBase::RemoveItem(unsigned long _num)
+{
+	if (_num >= itemList.size())return;
+	auto&& it = itemList.begin() + _num;
+	itemList.erase(it);
+}
+
+void SelectListBase::ClearItem()
+{
+	if (itemList.empty())return;
+	itemList.clear();
+}
+
+ChVec4 SelectListBase::GetStartRect()
+{
+	ChVec4 res;
+
+	res.left = startPosition.x;
+	res.top = startPosition.y;
+
+	res = UpdateRightBottomInRect(res);
+
+	return res;
+}
+
+ChVec4 SelectListBase::GetNextRect(const ChVec4& _rect)
+{
+	ChVec4 res = _rect;
+
+	res.left += alighSize.x;
+	res.top -= alighSize.y;
+
+	res = UpdateRightBottomInRect(res);
+
+	return res;
+}
+
+ChVec4 SelectListBase::UpdateRightBottomInRect(const ChVec4& _rect)
+{
+
+	ChVec4 res = _rect;
+
+	res.right = res.left + basePanelSize.x;
+	res.bottom = res.top - basePanelSize.y;
+
+	return res;
+}
+
+void SelectListBase::MoveAdd()
+{
+	if (drawCount < itemList.size())
+	{
+		if (nowSelectPanel == ((drawPos + drawCount - 1) % itemList.size()))
+			drawPos = (drawPos + 1) % itemList.size();
+	}
+
+	nowSelectPanel = (nowSelectPanel + 1) % itemList.size();
+}
+
+void SelectListBase::MoveSub()
+{
+	if (drawCount < itemList.size())
+	{
+		if (nowSelectPanel == drawPos)
+			drawPos = (drawPos + itemList.size() - 1) % itemList.size();
+	}
+
+	nowSelectPanel = (nowSelectPanel + itemList.size() - 1) % itemList.size();
 }
--- Src/SelectList/SelectList.h
+++ Src/SelectList/SelectList.h
@@ -1,47 +1,99 @@
 #pragma once
 
+#include"../Frames/MenuBase.h"
+
 class SelectListItemBase
 {
 public:
 
-	virtual void Draw(ChD3D11::Shader::BaseDrawSprite11& _drawer);
+	virtual ~SelectListItemBase(){}
 };
 
-enum class DrawDiraction
+enum class MoveDiraction
 {
-	LeftToRight,
-	TopToDown,
+	//êÇíº//
+	Vertical,
+	//êÖïΩ//
+	Horizontal
 };
 
 class SelectListBase
 {
 public:
 
+	void SetStartPosition(float _x, float _y);
+
+	void SetStartPosition(const ChVec2& _position);
+
+	void SetPanelSize(float _x, float _y);
+
+	void SetPanelSize(const ChVec2& _size);
+	
+	//StartPositionÇ©ÇÁÇÃëäëŒà íu//
+	void SetAlighSize(float _x, float _y);
+
+	//StartPositionÇ©ÇÁÇÃëäëŒà íu//
+	void SetAlighSize(const ChVec2& _size);
+
+	inline void SetDrawCount(int _drawCount) { drawCount = _drawCount; }
+
 public:
 
-	inline bool IsOnPanel() { return onPanelFlg; }
+	int GetNowSelect() { return nowSelectPanel; }
 
 public:
 
-	bool Update(ChWin::MouseController& _controller);
+	bool UpdateMouse();
+
+	void UpdateAction(MenuBase::ActionType _type);
+
+private:
+
+	void UpdateActionVertical(MenuBase::ActionType _type);
+
+	void UpdateActionHorizontal(MenuBase::ActionType _type);
 
 public:
 
 	void Draw(ChD3D11::Shader::BaseDrawSprite11& _drawer);
 
+	virtual void DrawPanel(ChD3D11::Shader::BaseDrawSprite11& _drawer, const ChVec4& _rect,ChPtr::Shared<SelectListItemBase> _drawItem, bool _isSelectPanel) = 0;
+
+public:
+
+	void AddItem(ChPtr::Shared<SelectListItemBase> _item);
+
+	void RemoveItem(ChPtr::Shared<SelectListItemBase> _item);
+
+	void RemoveItem(unsigned long _num);
+
+	void ClearItem();
+
 private:
 
-	bool onPanelFlg = false;
+	ChVec4 GetStartRect();
 
-	unsigned long drawCount = 0;
-	unsigned long drawPos = 0;
-	unsigned long nowSelectPanel = 0;
+	ChVec4 GetNextRect(const ChVec4& _rect);
+
+	ChVec4 UpdateRightBottomInRect(const ChVec4& _rect);
+
+private:
+
+	void MoveAdd();
+
+	void MoveSub();
+
+private:
+
+	int drawCount = 0;
+	int drawPos = 0;
+	int nowSelectPanel = 0;
 
 	ChVec2 startPosition;
-	ChVec2 basePanel;
+	ChVec2 basePanelSize;
 	ChVec2 alighSize;
 
+	MoveDiraction moveDirection = MoveDiraction::Vertical;
+
 	std::vector<ChPtr::Shared<SelectListItemBase>>itemList;
-
-
 };
