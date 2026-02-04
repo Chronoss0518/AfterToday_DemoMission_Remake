14b6786dc760ebe6b84168f16cb75e06a3ba1f0a
Chronoss0518 <sinyusuke5000@gmail.com>
2026-02-04 15:45:24 +0900

    CaterpillarDataクラスの追加

--- Src/BaseMecha/MechaParts.cpp
+++ Src/BaseMecha/MechaParts.cpp
@@ -24,6 +24,7 @@ std::map<std::wstring, std::function<ChPtr::Shared<PartsDataBase>(MechaParts&)>>
 	PARTS_DATA_CREATER(CameraData),
 	PARTS_DATA_CREATER(ScopeData),
 	PARTS_DATA_CREATER(WalkData),
+	PARTS_DATA_CREATER(CaterpillarData),
 	PARTS_DATA_CREATER(Aerodynamics),
 	PARTS_DATA_CREATER(MoveAcceleration),
 	PARTS_DATA_CREATER(NextPos),
@@ -479,6 +480,50 @@ void WalkData::SetPartsParameter(BaseMecha& _base, MechaPartsObject& _parts, Gam
 }
 
 void WalkData::SetPartsParameter(PartsParameters& _base)
+{
+	_base.walkData.movePower = movePow;
+	_base.walkData.rotatePower = rotatePow;
+	_base.walkData.jumpPower = jumpPow;
+}
+
+void CaterpillarData::RemoveParameter(BaseMecha& _base)
+{
+	auto&& walk = GetComponent<BaseMechaMoveComponent>(_base);
+
+	walk->SetMovePow(0.0f);
+	walk->SetRotatePow(0.0f);
+	walk->SetJumpPow(0.0f);
+}
+
+unsigned long CaterpillarData::Deserialize(const ChCpp::TextObject<wchar_t>& _text, const unsigned long _textPos)
+{
+	movePow = ChStr::GetNumFromText<float>(_text.GetTextLine(_textPos).c_str());
+	rotatePow = ChStr::GetNumFromText<float>(_text.GetTextLine(_textPos + 1).c_str());
+	jumpPow = ChStr::GetNumFromText<float>(_text.GetTextLine(_textPos + 2).c_str());
+	return _textPos + 3;
+}
+
+std::wstring CaterpillarData::Serialize()
+{
+	std::wstring res = L"";
+
+	res += std::to_wstring(movePow) + L"\n";
+	res += std::to_wstring(rotatePow) + L"\n";
+	res += std::to_wstring(jumpPow) + L"\n";
+
+	return res;
+}
+
+void CaterpillarData::SetPartsParameter(BaseMecha& _base, MechaPartsObject& _parts, GameFrame* _frame)
+{
+	auto&& walk = GetComponent<TankMoveComponent>(_base);
+
+	walk->SetMovePow(movePow);
+	walk->SetRotatePow(rotatePow);
+	walk->SetJumpPow(jumpPow);
+}
+
+void CaterpillarData::SetPartsParameter(PartsParameters& _base)
 {
 	_base.walkData.movePower = movePow;
 	_base.walkData.rotatePower = rotatePow;
