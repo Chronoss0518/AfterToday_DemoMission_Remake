9b10c7a27d1b7202d69a2833ea465d2883a399fa
Chronoss0518 <sinyusuke5000@gmail.com>
2026-03-17 19:53:25 +0900

    不要になった処理の削除

--- Src/BaseMecha/FunctionComponent/BoostComponent.h
+++ Src/BaseMecha/FunctionComponent/BoostComponent.h
@@ -2,17 +2,19 @@
 
 #include"FunctionComponent.h"
 
+#include"../BoostDirection.h"
 
 class EnergyComponent;
 class BoostBrust;
 
 class BoostComponent:public FunctionComponent
 {
-private:
+public:
 
-	struct Data;
-	struct BoostData;
-	struct AvoidData;
+	struct UseBoostData
+	{
+		BoostBrust* data = nullptr;
+ 	};
 
 public:
 
@@ -26,114 +28,19 @@ public:
 
 	void Update()override;
 
-	void BoostDrawBegin();
-
-	void BoostDrawEnd();
-
-private:
-
-	void UpdateInputFunction(Data& _data);
-
-	void UpdateModelFunction(Data& _data);
-
-	void UpdateBoost(Data& _data, const ChLMat& _nowTargetPoster, EnergyComponent& _energy);
-
-	void UpdateAvoid(Data& _data, const ChLMat& _nowTargetPoster, EnergyComponent& _energy);
-
 public:
 
 	void SetBoostAvoidWait(const unsigned long _avoidWait, InputName _avoidType);
 
-private:
-
-	unsigned long GetUseEnergy(AvoidData& _data);
-
-	unsigned long GetUseEnergy(BoostData& _data, bool _avoidFlg);
-
 public:
 
-	void AddBoostWhereAvoidName(ChPtr::Shared<ChCpp::FrameObject<wchar_t>> _boost, InputName _avoidType);
+	void AddBoostData(BoostBrust* _data);
 
-	void AddBoostWhereBoostName(ChPtr::Shared<ChCpp::FrameObject<wchar_t>> _boost, InputName _boostType);
-
-	void AddBoostPow(const float _boostPow, InputName _boostType);
-
-	void AddBoostUseEnergy(const unsigned long _boostUseEnergy, InputName _boostType);
-
-	void AddBoostAvoidPow(const float _avoidPow, InputName _avoidType);
-
-	void AddBoostAvoidUseEnergy(const unsigned long _avoidUseEnergy, InputName _avoidType);
-
-public:
-
-	void SubBoostWhereAvoidName(ChPtr::Shared<ChCpp::FrameObject<wchar_t>> _boost, InputName _avoidType);
-		 
-	void SubBoostWhereBoostName(ChPtr::Shared<ChCpp::FrameObject<wchar_t>> _boost, InputName _boostType);
-		 
-	void SubBoostPow(const float _boostPow, InputName _boostType);
-		 
-	void SubBoostUseEnergy(const unsigned long _boostUseEnergy, InputName _boostType);
-		 
-	void SubBoostAvoidPow(const float _avoidPow, InputName _avoidType);
-		 
-	void SubBoostAvoidUseEnergy(const unsigned long _avoidUseEnergy, InputName _avoidType);
+	void SubBoostData(BoostBrust* _data);
 
 private:
 
 	static float& GetBoostUpPowerRegister() { static float regist = 0.3f; return regist; }
 
-	enum class BoostDirection : unsigned char
-	{
-		Front,
-		Back,
-		Right,
-		Left,
-		Up,
-		Down
-	};
-
-	struct BoostData
-	{
-
-		InputName name;
-		float pow = 0.0f;
-		unsigned long useEnergy = 0;
-
-		bool testUseFlg = false;
-
-	};
-
-	struct AvoidData
-	{
-
-		InputName name;
-		float pow = 0.0f;
-		unsigned long useEnergy = 0;
-		unsigned long wait = 0;
-
-		bool useFlg = false;
-		unsigned long nowWaitTime = 0;
-
-		bool testUseFlg = false;
-
-	};
-
-	struct Data
-	{
-		ChVec3 direction;
-
-		BoostData boost;
-		AvoidData avoid;
-
-		float nowBoostPow = 0.0f;
-
-		std::vector<ChPtr::Shared<ChCpp::FrameObject<wchar_t>>>boostObject;
-
-	};
-
-	float boostRotation = 0.0f;
-
-	Data data[6];
-
-	std::vector<ChPtr::Shared<BoostBrust>>boostBrustList;
+	std::vector<BoostBrust*>boostBrustList;
 };
