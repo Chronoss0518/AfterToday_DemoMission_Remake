410034d378a8eec61245f9b7a629c64814e67a9b
Chronoss0518 <sinyusuke5000@gmail.com>
2026-03-18 14:42:30 +0900

    Boostのサイズを管理するコンポーネントを作成

---
+++ Src/BaseMecha/MechaPartsObjectFunction/BoostFunction.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include"../MechaPartsObject.h"
+
+class BoostBrust;
+
+class BoostFunction :public ChCpp::BaseComponent
+{
+public:
+
+	void Update()override;
+
+	void DrawBegin()override;
+
+	void DrawEnd()override;
+
+public:
+
+	inline void SetTargetBoostObject(ChPtr::Shared<ChCpp::TransformObject<wchar_t>> _targetObject)
+	{
+		if (_targetObject == nullptr)return;
+		targetBoostObject = _targetObject;
+	}
+
+	void SetBoostScaling();
+
+	void SetAvoidScaling();
+
+private:
+
+	ChPtr::Shared<ChCpp::TransformObject<wchar_t>>targetBoostObject = nullptr;
+
+	float nowScaling = 0.0f;
+
+};
