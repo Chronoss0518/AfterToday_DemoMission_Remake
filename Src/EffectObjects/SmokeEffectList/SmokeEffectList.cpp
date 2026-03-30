cc1cee0fa597ba7106ad97d29a705dbc3b761933
Chronoss0518 <sinyusuke5000@gmail.com>
2026-03-30 22:16:08 +0900

    引数のDeviceを利用するように修正

--- Src/EffectObjects/SmokeEffectList/SmokeEffectList.cpp
+++ Src/EffectObjects/SmokeEffectList/SmokeEffectList.cpp
@@ -21,7 +21,7 @@ void SmokeEffectList::Init(ID3D11Device* _device, const unsigned long _maxCount,
 	gameEndFlg = false;
 	effectShader = ChPtr::Make_S<EffectObjectShader>();
 
-	effectShader->Init(AppIns().GetDirect3D11().GetDevice(), _maxCount);
+	effectShader->Init(_device, _maxCount);
 	effectMoveDataList.resize(_maxCount);
 
 	effectShader->SetEffectTexture(TEXTURE_DIRECTORY(L"SircleTexture.png"), 1, 1);
