878d71804d04d18109df3196ec0443fa392ff3bc
Chronoss0518 <sinyusuke5000@gmail.com>
2026-04-09 16:59:45 +0900

    出力されるConnectionRotateの4列目に1.0fが入力されていたため修正

--- Src/BaseMecha/MechaPartsData/NextPosData.cpp
+++ Src/BaseMecha/MechaPartsData/NextPosData.cpp
@@ -10,12 +10,19 @@ void NextPosData::SetObjectPos(BaseMecha& _base, MechaPartsObject& _parts, ChPtr
 {
 	auto&& mechaParts = LookObj<MechaParts>();
 
-	ChVec3 cross = ChVec3::GetCross(connectionRotateUp, connectionRotateNormal);
+	ChVec4 tmpUp = connectionRotateUp;
+	tmpUp.w = 0.0f;
+	tmpUp.Normalize();
+	ChVec4 tmpNormal = connectionRotateNormal;
+	tmpNormal.w = 0.0f;
+	tmpNormal.Normalize();
 
+	ChVec4 cross = ChVec3::GetCross(tmpUp, tmpNormal);
+	cross.w = 0.0f;
 	ChLMat lmat;
 	lmat.m[0].Set(cross.val);
-	lmat.m[1].Set(connectionRotateUp.val);
-	lmat.m[2].Set(connectionRotateNormal.val);
+	lmat.m[1].Set(tmpUp.val);
+	lmat.m[2].Set(tmpNormal.val);
 
 	mechaParts->AddPosition(connectionName, _targetObject, lmat, type, maxWeight);
 }
